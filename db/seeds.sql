c8ea8632068db8e58de04b0a838c39a8c8a397d3
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-05-29 10:19:00 -0700

    added files
    added files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ("Eggers Can't Be Cheesers (with fried egg & cheese)");
+INSERT INTO burgers (name) VALUES ("Gourdon-Hamsey Burger (comes with squash & ham)");
+INSERT INTO burgers (name, devoured) VALUES ("Chipotle Off the Old Block Burger", true);
+INSERT INTO burgers (name, devoured) VALUES ("Bleu by You Burger (with locally sourced bleu cheese)", true);
+INSERT INTO burgers (name, devoured) VALUES ("Kales From the Crypt Burger", true);
+INSERT INTO burgers (name) VALUES ("Thank God It's Fried Egg Burger");
